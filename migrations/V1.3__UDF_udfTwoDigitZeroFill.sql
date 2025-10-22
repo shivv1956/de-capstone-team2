39325a2aaf475e9f173d43a4bcc8cccba168ec6f
Shiva Shankar P <shivap@phdata.io>
2025-10-22 10:17:30 +0530

    Dev - Starting with clean branch

--- migrations/V1.3__UDF_udfTwoDigitZeroFill.sql
+++
@@ -1,7 +0,0 @@
-CREATE OR REPLACE FUNCTION ${CURRENT_SCHEMA}.udfTwoDigitZeroFill(number INT)
-RETURNS CHAR(2)
-LANGUAGE SQL
-AS
-$$
-    LPAD(CAST(number AS STRING), 2, '0')
-$$;
--- migrations/V3__view_vAssocSeqLineItems.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW ${CURRENT_SCHEMA}.vAssocSeqLineItems AS
-SELECT
-  OrderNumber,
-  LineNumber,
-  Model
-FROM
-  ${CURRENT_SCHEMA}.vDMPrep
-WHERE
-  (FiscalYear = '2013');
